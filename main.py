b03fb990bda5ae5cbc6e57a3e125d5dbf208666d
mcostabile <marcedcostabile@gmail.com>
2022-08-10 23:53:55 -0300

    removing test info

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from mate import Mate
 
 
 def main():
-    m1 = Mate("mate1")
-    m1.add_busy_range(TimeRange(start_time="08:00", end_time="14:00"))
-    m2 = Mate("mate2")
-    m2.add_busy_range(TimeRange(start_time="10:00", end_time="17:00"))
-    m3 = Mate("mate3")
-    m3.add_busy_range(TimeRange(start_time="20:00", end_time="23:00"))
+    mate = Mate("mate")
+    mate.add_busy_range(TimeRange(start_time="09:00", end_time="13:00"))
 
     Mate.show_available_time()
 
